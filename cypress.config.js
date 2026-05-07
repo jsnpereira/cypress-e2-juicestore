4c25d8e3d7d0e66020706442d58c02f6cb1f5de9
Jeison Da silva Pereira <jeisonpereira@MacBook-Pro-de-Jeison.local>
2026-05-07 20:52:49 -0300

    Add google drive configurations

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,14 @@
 const {defineConfig} = require('cypress')
+const { report } = require('node:process')
 
 module.exports = defineConfig({
+    reporter: 'mochawesome',
+    reporterOptions:{
+        reportDir: 'cypress/reports',
+        overwrite: false,
+        html: true,
+        json: true
+    },
     e2e: {
         baseUrl: 'http://localhost:3000',
         viewportWidth: 1280,
