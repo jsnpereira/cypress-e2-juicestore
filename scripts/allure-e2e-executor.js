2c97876a732a1886f5e0b24a10f24da9984631e6
Jeison Da silva Pereira <jeisonpereira@MacBook-Pro-de-Jeison.local>
2026-05-21 22:21:21 -0300

    improve config the allure report

---
+++ scripts/allure-e2e-executor.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+fs.mkdirSync("allure-results",  { recursive: true });
+
+fs.writeFileSync("allure-results/executor.json",
+    JSON.stringify({
+        name: "GitHub Actions",
+        type: "e2e",
+        buildOrder: Number(process.env.GITHUB_RUN_NUMBER),
+        buildName: `Build #${process.env.GITHUB_RUN_NUMBER}`,
+        buildUrl: `https://${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`,
+        reportUrl: `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io/cypress-e2-juicestore`,
+
+    }, null, 2));
+
+console.log("executor.json criado");
