71a88e364601c40999721cdd908043954e9403a7
Jeison Da silva Pereira <jeisonpereira@MacBook-Pro-de-Jeison.local>
2026-05-21 22:55:21 -0300

    fix the github link in executor.js and remove the comment e2e.yml

--- scripts/allure-e2e-executor.js
+++ scripts/allure-e2e-executor.js
@@ -8,8 +8,8 @@ fs.writeFileSync("allure-results/executor.json",
         type: "e2e",
         buildOrder: Number(process.env.GITHUB_RUN_NUMBER),
         buildName: `Build #${process.env.GITHUB_RUN_NUMBER}`,
-        buildUrl: `https://github.com/${process.env.GITHUB_REPOSITORY_OWNER}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`,
-        reportUrl: `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io/cypress-e2-juicestore`,
+        buildUrl: `https://github.com/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`,
+        reportUrl: `https://github.com/${process.env.GITHUB_REPOSITORY_OWNER}/cypress-e2-juicestore`,
 
     }, null, 2));
 
