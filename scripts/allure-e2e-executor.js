a628260fc4fab172c4297a28f047faa28d36e2c0
Jeison Da silva Pereira <jeisonpereira@MacBook-Pro-de-Jeison.local>
2026-05-21 22:44:44 -0300

    Fix e2e script and link build in executor.js

--- scripts/allure-e2e-executor.js
+++ scripts/allure-e2e-executor.js
@@ -8,7 +8,7 @@ fs.writeFileSync("allure-results/executor.json",
         type: "e2e",
         buildOrder: Number(process.env.GITHUB_RUN_NUMBER),
         buildName: `Build #${process.env.GITHUB_RUN_NUMBER}`,
-        buildUrl: `https://${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`,
+        buildUrl: `https://github.com/${process.env.GITHUB_REPOSITORY_OWNER}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`,
         reportUrl: `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io/cypress-e2-juicestore`,
 
     }, null, 2));
